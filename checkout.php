116431300d7905efc5218996c4d5356a0a0ea00a
Lilian Mutisya <57066022+Lilo13@users.noreply.github.com>
2023-11-26 16:39:04 -0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,42 +1,85 @@
 <?php
+
 session_start();
 
-$conn = mysqli_connect("localhost", "root", "", "fast_travel");
+require_once "config.php";
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Check if the form is submitted
+// Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["checkout"])) {
 
-    // Create a unique order ID (you can customize this based on your needs)
-    $order_id = uniqid();
+    // Create a booking ID
+    $booking_id = uniqid();
 
     if (isset($_SESSION["tours"]) && is_array($_SESSION["tours"])) {
-        // Your existing code for updating tour capacities
         foreach ($_SESSION["tours"] as $key => $value) {
             $tour_id = $value["tour_id"];
             $pple_number = $value["pple_number"];
             
-            // Update the tour capacity in the database
+            // Update tour capacity in the database
             $update_query = "UPDATE tours SET tour_capacity = tour_capacity - ? WHERE tour_id = ?";
             $stmt = mysqli_prepare($conn, $update_query);
             mysqli_stmt_bind_param($stmt, "ii", $pple_number, $tour_id);
             $result = mysqli_stmt_execute($stmt);
             
             if (!$result) {
+                echo "Error updating tour capacity. Please contact support.";
                 error_log("Error updating record: " . mysqli_error($conn) . ". Query: " . $update_query);
             }
-        }
-        
-        // Clear the shopping cart after checkout
-        unset($_SESSION["tours"]);
+        }      
+        // Display the invoice on the checkout page
+        echo <<<HTML
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Invoice</title>
+                </head>
+                    
+                <body>
+                    <div>
+                        <h1>Invoice</h1>
+                        <p>Booking ID: $booking_id</p>
+                        
+                        <table border="1">
+                        <tr>
+                            <th>Location</th>
+                            <th>Guests</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                            </tr>
+                            
+                HTML;
 
-        header("Location: confirmation.php?order_id=" . $order_id);
-        exit();
+                        $total = 0;
+                        foreach ($_SESSION["tours"] as $key => $value) {
+                            echo "<tr>";
+                            echo "<td>{$value['location']}</td>";
+                            echo "<td>{$value['pple_number']}</td>";
+                            echo "<td>{$value['price']}</td>";
+                            echo "<td>" . ($value['pple_number'] * $value['price']) . "</td>";
+                            echo "</tr>";
+                            $total += $value['pple_number'] * $value["price"];
+                        }
+                        
+                        echo <<<HTML
+                        <tr>
+                            <td colspan="3" align="right">Total</td>
+                            <td>$total</td>
+                        </tr>
+                    </table>
+                    
+                    <p>Thank you for booking with us!</p>
+                </div>
+            </body>
+            </html>
+                
+HTML;        
+        // Clear shopping cart after checkout
+        unset($_SESSION["tours"]);
+    
     } else {
-        // Handle the case when there are no tours in the session
+        // If no tours in the session
         echo "No tours in the shopping cart.";
     }
 } else {
@@ -46,48 +89,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["checkout"])) {
 
 mysqli_close($conn);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoice</title>
-    <!-- Add your CSS styles if needed -->
-</head>
-<body>
-    <div>
-        <h1>Invoice</h1>
-        <p>Order ID: <?php echo $order_id; ?></p>
-
-        <table border="1">
-            <tr>
-                <th>Location</th>
-                <th>Guests</th>
-                <th>Price</th>
-                <th>Total</th>
-            </tr>
-            <?php
-            $total = 0;
-            foreach ($_SESSION["tours"] as $key => $value) {
-                ?>
-                <tr>
-                    <td><?php echo $value["location"]; ?></td>
-                    <td><?php echo $value["pple_number"]; ?></td>
-                    <td><?php echo $value["price"]; ?></td>
-                    <td><?php echo $value["pple_number"] * $value["price"]; ?></td>
-                </tr>
-                <?php
-                $total += $value["pple_number"] * $value["price"];
-            }
-            ?>
-            <tr>
-                <td colspan="3" align="right">Total</td>
-                <td><?php echo $total; ?></td>
-            </tr>
-        </table>
-
-        <p>Thank you for booking with us!</p>
-    </div>
-</body>
-</html>
